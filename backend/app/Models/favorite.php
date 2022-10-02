4dce8613cbfd5a898a5524173f6478cf523a23a7
Maytham Ghaly <maythamghaly@gmail.com>
2022-10-02 21:20:44 +0300

    backend: adding db tables

--- backend/app/Models/favorite.php
+++ backend/app/Models/favorite.php
@@ -4,11 +4,9 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Foundation\Auth\User as AuthUser;
 
-class user extends Model
+class favorite extends Model
 {
     use HasFactory;
-    
-
+   
 }
---
+++ backend/database/migrations/2022_10_02_154133_create_chats_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateChatsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('chats', function (Blueprint $table) {
+            $table->id();
+            $table->foreignID("sender_id")->references('id')->on("users");
+            $table->string("message");
+            $table->foreignID("receiver_id")->references('id')->on("users");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('chats');
+    }
+}
