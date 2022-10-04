c7b19835ebdd599b365bc4811c6975668c0ebfbc
Maytham Ghaly <maythamghaly@gmail.com>
2022-10-04 23:35:02 +0300

    frontend: fixing chat js

--- frontend/JS.js/sendchat.js
+++ frontend/JS.js/sendchat.js
@@ -1,6 +1,17 @@
-export const Chats = () => {
-    if (document.getElementById('send-msg'))
-    {
-        
+export const sendChats = async (receiver_id, chat_message) => {
+    const url = `http://127.0.0.1:8000/api/add_messages`;
+
+    let data = ({
+        receiver_id,
+        message: chat_message
+    })
+    const msg = await axios.post(url, data, { headers: { 'Authorization': `Bearer ${localStorage.getItem(`token`)}` } });
+    console.log("yes")
+    const user_message = document.createElement("li");
+    user_message.classList.add("sender");
+    user_message.innerHTML = chat_message;
+    document.getElementById("chat-box").append(user_message)
 }
-}
+
+
+
