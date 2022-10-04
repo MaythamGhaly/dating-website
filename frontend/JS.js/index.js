f7b8238b64488669c0994ed5b41f37ad8fd15b9a
Maytham Ghaly <maythamghaly@gmail.com>
2022-10-04 23:33:52 +0300

    frontend: fixing classes

--- frontend/JS.js/index.js
+++ frontend/JS.js/index.js
@@ -3,10 +3,13 @@ import {PageLocations} from "./page-location.js";
 import {LoginValidation} from "./login.js";
 import {GetUsers} from "./getusers.js";
 import {getFavorite} from "./getfavorite.js"
+import {getBlock} from "./get-block.js"
+
 
 
 PageLocations();
 LoginValidation();
 GetUsers();
 getFavorite();
+getBlock();
 
