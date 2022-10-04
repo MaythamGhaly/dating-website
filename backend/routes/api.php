e451a74958da0fda469d2ff4f1bd92aa81ebb91a
Maytham Ghaly <maythamghaly@gmail.com>
2022-10-04 23:30:24 +0300

    backend: adding api

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -10,12 +10,17 @@ use Illuminate\Support\Facades\Auth;
     Route::group(['middleware' => 'user.role'], function () {
 
         Route::post('/logout', [AuthController::class, "logout"])->name("logout");
-        Route::post('/get_favorites', [FeedController::class, "getFavorites"])->name("get-favorites");
-        Route::post('/block_user/{id?}', [FeedController::class, "blockUser"])->name("block-user");
-        Route::post('/add_favorite/{id?}', [FeedController::class, "addFavorite"])->name("add-favorite");
+        Route::get('/get_favorites', [FeedController::class, "getFavorites"])->name("get-favorites");
+        Route::get('/get_blocks', [FeedController::class, "getBlocks"])->name("get-blocks");
+        Route::get('/block_user/{id?}', [FeedController::class, "blockUser"])->name("block-user");
+        Route::get('/add_favorite/{id?}', [FeedController::class, "addFavorite"])->name("add-favorite");
         Route::get('/get_users', [FeedController::class, "getUser"])->name("get_users");
         Route::get('/get_messages', [FeedController::class, "getReceiveMessages"])->name("get-receive-messages");
         Route::post('/add_messages', [FeedController::class, "addMessages"])->name("add-messages");
+        Route::get('/delete_blocks/{id?}', [FeedController::class, "deletBlocks"])->name("delet-blocks");
+        Route::get('/delete_favorite/{id?}', [FeedController::class, "deletFavorite"])->name("delet-favorite");
+
+
 
 
 
